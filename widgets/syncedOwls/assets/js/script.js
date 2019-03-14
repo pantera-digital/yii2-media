3206940fc82d6fdf403699130c3734be66a490e2
serega <r.s.i@bk.ru>
2019-03-14 18:57:29 +1000

    реализация приемки дата атрибутов для их установки в JS плагины

--- widgets/syncedOwls/assets/js/script.js
+++ widgets/syncedOwls/assets/js/script.js
@@ -1,20 +1,28 @@
 function syncedOwls(id) {
     const container = $('#' + id);
-    container.find('[data-fancybox="images"]').fancybox({
+    var fancyboxOptions = container.data('fancyboxoptions');
+    var fancyboxDefaults = {
         loop: false,
         thumbs: {
             autoStart: true,
             axis: 'x'
         }
-    });
+    };
+
+    if (fancyboxOptions) {
+        $.extend(true, fancyboxDefaults, fancyboxOptions);
+    }
+
+    container.find('[data-fancybox="images"]').fancybox(fancyboxDefaults);
     let $sync1 = container.find('.synced-carousel-main'),
         $sync2 = container.find('.synced-carousel-thumbs'),
         flag = false,
         duration = 300;
 
 
-    $sync1
-        .owlCarousel({
+    var mainCarouselOptions = container.data('maincarouseloptions'),
+        thumbsCarouselOptions = container.data('thumbscarouseloptions'),
+        mainCarouselDefaults = {
             items: 1,
             margin: 0,
             nav: true,
@@ -22,7 +30,31 @@ function syncedOwls(id) {
                 '<svg xmlns="http://www.w3.org/2000/svg" width="8.031" height="12.969" viewBox="0 0 8.031 12.969"><path d="M177.745,252.782q-3.093-2.772-6.185-5.544c-0.848-.76-2.114.475-1.262,1.239q2.752,2.466,5.5,4.933-2.766,2.552-5.531,5.107a0.884,0.884,0,0,0,1.262,1.238l6.212-5.734A0.867,0.867,0,0,0,177.745,252.782Z" transform="translate(-170 -247)"/></svg>'
             ],
             dots: false,
-        })
+        },
+        
+        thumbsCarouselDefaults = {
+            margin: 20,
+            items: 4,
+            nav: false,
+            center: false,
+            dots: false,
+            onInitialized: function () {
+                container.find('.synced-carousel-thumbs .owl-item').eq(0).addClass('current');
+            }
+        };
+
+        if (mainCarouselOptions) {
+            $.extend(true, mainCarouselDefaults, mainCarouselOptions);
+        }
+
+        if (thumbsCarouselOptions) {
+            $.extend(true, thumbsCarouselDefaults, thumbsCarouselOptions);
+        }
+
+
+
+    $sync1
+        .owlCarousel(mainCarouselDefaults)
         .on('changed.owl.carousel', function (el) {
             var index = el.item.index;
             container.find('.synced-carousel-thumbs .owl-item').eq(index).addClass('current').siblings().removeClass('current')
@@ -36,16 +68,7 @@ function syncedOwls(id) {
         });
 
     $sync2
-        .owlCarousel({
-            margin: 20,
-            items: 4,
-            nav: false,
-            center: false,
-            dots: false,
-            onInitialized: function () {
-                container.find('.synced-carousel-thumbs .owl-item').eq(0).addClass('current');
-            }
-        })
+        .owlCarousel(thumbsCarouselDefaults)
         .on('click', '.owl-item', function () {
             $sync1.trigger('to.owl.carousel', [$(this).index(), duration, true]);
 
